913bb18caa51ba33833f57bfa1906c69692abe61
Sana Parveen <misana348@gmail.com>
2024-02-22 16:26:02 +0530

    Layout navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-2xl text-center">Hey</div>
-  )
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text -2xl text-center" >hello</h1>
+    </main>
+    )
 }
