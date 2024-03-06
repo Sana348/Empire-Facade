e17682977001b518c875bd60153b0b38080610a2
Sana Parveen <misana348@gmail.com>
2024-03-06 22:28:44 +0530

    Final project commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
-"use client";
 import React from "react";
-import {SparklesCore } from '@/components/ui/sparkles';
+import { SparklesCore } from "@/components/ui/sparkles";
 
 const AboutPage: React.FC = () => {
   return (
@@ -16,9 +15,17 @@ const AboutPage: React.FC = () => {
           particleColor="#FFFFFF"
         />
       </div>
-      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
-        Build great products
-      </h1>
+      <div className="container mx-auto px-4 py-12 text-white relative z-20">
+        <p className="text-xl mt-8 mx-auto max-w-xl text-center">
+          At FitnessWell, we are dedicated to helping you achieve your health and wellness goals. Our mission is to provide top-quality fitness classes, personalized training, and a supportive community to empower you on your journey to a healthier lifestyle.
+        </p>
+        <p className="text-xl mt-8 mx-auto max-w-xl text-center">
+          Whether you are a beginner looking to kickstart your fitness journey or an experienced athlete striving for new heights, our team of expert trainers and coaches are here to guide and motivate you every step of the way.
+        </p>
+        <p className="text-xl mt-8 mx-auto max-w-xl text-center mb-16">
+          Join us today and experience the transformative power of fitness and wellness at FitnessWell.
+        </p>
+      </div>
     </div>
   );
 };
